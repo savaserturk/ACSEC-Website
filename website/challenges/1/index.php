0e9ff5b61d991d0fc00e0947abbbeb77ba70ed59
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-09-19 23:00:58 -0400

    add code submission box to temporary challenge page

---
+++ website/challenges/1/index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <title>1 - Gates</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js?skin=default"></script>
+  </head>
+  <body>
+    <h1>Challenge 1: Gates</h1>
+    <p>Posted on Apr 29 2019</p>
+    <p>Difficulty: Easy</p>
+    <p>
+      Given 2 inputs (each a 0 or a 1), show the outputs of each of the following logic gates: AND, OR, and XOR. This is a good opportunity to look up the bitwise operators if you aren't familiar with them.
+    </p>
+    <h3>Example 1:</h3>
+    <div class="example-code">
+      <p>Enter two inputs: 0 1</p>
+      <p>AND: 0</p>
+      <p>OR:  1</p>
+      <p>XOR: 1</p>
+    </div>
+    <h3>Example 2:</h3>
+    <div class="example-code">
+      <p>Enter two inputs: 1 1</p>
+      <p>AND: 1</p>
+      <p>OR:  1</p>
+      <p>XOR: 0</p>
+    </div>
+    <?php
+      if (isset($_POST['submit'])) {
+        echo '<div class="content">';
+        echo '  <pre class="prettyprint linenums lang-java">' . $_REQUEST['code'] . '</pre>';
+        echo '</div>';
+      }
+      else {
+          echo '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">';
+          echo '  <div id="code-submission-panel">';
+          echo '    <textarea name="code" rows=4 cols=50 placeholder="Enter your solution here..."></textarea>';
+          echo '    <input type="submit" name="submit" value="Submit">';
+          echo '  </div>';
+          echo '</form>';
+      }
+    ?>
+  </body>
+</html>
