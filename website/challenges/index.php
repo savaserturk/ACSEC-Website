bb0e15b3abda491731ab0da716f386f476e2a93d
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-10-19 20:09:13 -0400

    Hide post-dated challenges in challenge list

--- website/challenges/index.php
+++ website/challenges/index.php
@@ -22,45 +22,51 @@ $memberId = 0;
       $query = $db->prepare('SELECT * FROM Challenge ORDER BY ChallengeId DESC');
       $query->execute();
       foreach ($query as $row) {
-        $challengeId = $row['ChallengeId'];
-        $title = $row['Title'];
-        $date = $row['DatePosted'];
+        date_default_timezone_set('America/Toronto');
+        $today = date("Y-m-d H:i:s");
 
-        // Difficulties are stored in the database as integers from 1-3, so we must convert it into a more useful form. We'll set 1 as Easy and 3 as Hard.
-        $difficulty = $row['Difficulty'];
-        if ($difficulty == 1)
-          $difficulty = 'Easy';
-        else if ($difficulty == 2)
-          $difficulty = 'Medium';
-        else if ($difficulty == 3)
-          $difficulty = 'Hard';
+        // Don't show challenges that have been posted ahead of time.
+        if ($row['DatePosted'] <= $today) {
+          $challengeId = $row['ChallengeId'];
+          $title = $row['Title'];
+          $date = $row['DatePosted'];
 
-        // Count the number of members that have submitted a solution for this challenge.
-        $submissionCountQuery = $db->prepare('SELECT * FROM Submission WHERE ChallengeId = ?');
-        $submissionCountQuery->execute([$challengeId]);
-        $submissionCount = $submissionCountQuery->rowCount();
+          // Difficulties are stored in the database as integers from 1-3, so we must convert it into a more useful form. We'll set 1 as Easy and 3 as Hard.
+          $difficulty = $row['Difficulty'];
+          if ($difficulty == 1)
+            $difficulty = 'Easy';
+          else if ($difficulty == 2)
+            $difficulty = 'Medium';
+          else if ($difficulty == 3)
+            $difficulty = 'Hard';
 
-        // Check whether this member has submitted a solution for this challenge already.
-        $submittedQuery = $db->prepare('SELECT * FROM Submission WHERE ChallengeId = ? AND MemberId = ?');
-        $submittedQuery->execute([$challengeId, $memberId]);
-        $hasSubmitted = $submittedQuery->rowCount() > 0;
+          // Count the number of members that have submitted a solution for this challenge.
+          $submissionCountQuery = $db->prepare('SELECT * FROM Submission WHERE ChallengeId = ?');
+          $submissionCountQuery->execute([$challengeId]);
+          $submissionCount = $submissionCountQuery->rowCount();
 
-        echo '<li>';
-        echo '  <a href="' . $challengeId . '/">';
+          // Check whether this member has submitted a solution for this challenge already.
+          $submittedQuery = $db->prepare('SELECT * FROM Submission WHERE ChallengeId = ? AND MemberId = ?');
+          $submittedQuery->execute([$challengeId, $memberId]);
+          $hasSubmitted = $submittedQuery->rowCount() > 0;
 
-        if ($hasSubmitted)
-          echo '  <div class="challenge-item submitted">';
-        else
-          echo '  <div class="challenge-item">';
+          echo '<li>';
+          echo '  <a href="' . $challengeId . '/">';
 
-        echo '      <h3 class="challenge-number">' . $challengeId . '</h3>';
-        echo '      <h3 class="challenge-title">' . $title . '</h3>';
-        echo '      <h3 class="challenge-difficulty">' . $difficulty . '</h3>';
-        echo '      <h3 class="challenge-submissions">Submissions: ' . $submissionCount . '</h3>';
-        echo '      <h3 class="challenge-date">' . $date . '</h3>';
-        echo '    </div>';
-        echo '  </a>';
-        echo '</li>';
+          if ($hasSubmitted)
+            echo '  <div class="challenge-item submitted">';
+          else
+            echo '  <div class="challenge-item">';
+
+          echo '      <h3 class="challenge-number">' . $challengeId . '</h3>';
+          echo '      <h3 class="challenge-title">' . $title . '</h3>';
+          echo '      <h3 class="challenge-difficulty">' . $difficulty . '</h3>';
+          echo '      <h3 class="challenge-submissions">Submissions: ' . $submissionCount . '</h3>';
+          echo '      <h3 class="challenge-date">' . $date . '</h3>';
+          echo '    </div>';
+          echo '  </a>';
+          echo '</li>';
+        }
       }
 
       ?>
