56a8faa7ed4b3cb3c56402fabc34692e4ed96f3f
arty <arty@123.com>
2019-11-06 20:26:59 -0500

    added stylesheet to login.php

--- website/signin/login.php
+++ website/signin/login.php
@@ -30,8 +30,16 @@
 <!DOCTYPE html>
 <html>
   <head>
+
+     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
     <title>Sign In</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <!--<link rel="stylesheet" type="text/css" href="style.css">-->
   </head>
   <body>
     <h1>Sign In</h1>
