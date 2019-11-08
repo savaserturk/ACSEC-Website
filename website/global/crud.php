f170b3de0516bf9d80fc4ae7f3859f1c912f0e5d
arty <arty@123.com>
2019-11-08 11:23:13 -0500

    changed login form to use crud

--- website/global/crud.php
+++ website/global/crud.php
@@ -40,5 +40,30 @@
       header("Location:../register.php?status=no");
     }
   }
+  if(isset($_POST['login'])){
+  	
+
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      // username and password sent from form
+      $username = $_POST['email'];
+      $password = $_POST['password'];
+      $query1 = $db->prepare('SELECT Password, MemberId FROM Member WHERE Email = ?');
+      $query1->execute([$username]);
+      $count = $query1->rowCount();
+      $row = $query1->fetch();
+      $password_h = $row['Password'];
+
+      // If result matched $myusername and $mypassword, table row must be 1 row
+      if($count == 1 && password_verify($password, $password_h)) {
+         $_SESSION['username'] = $username;
+
+         header("Location: index.html");
+      }else {
+         $error = "Your Login Name or Password is invalid!";
+         echo $error;
+      }
+   }
+  }
 
 ?>
--- website/signin/login.php
+++ website/signin/login.php
@@ -2,29 +2,31 @@
    require('../global/database.php');
    session_start();
 
-   $db = Database::getConnection();
+   // $db = Database::getConnection();
 
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
+   // if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-      // username and password sent from form
-      $username = $_POST['email'];
-      $password = $_POST['password'];
-      $query1 = $db->prepare('SELECT Password, MemberId FROM Member WHERE Email = ?');
-      $query1->execute([$username]);
-      $count = $query1->rowCount();
-      $row = $query1->fetch();
-      $password_h = $row['Password'];
+   //    // username and password sent from form
+   //    $username = $_POST['email'];
+   //    $password = $_POST['password'];
+   //    $query1 = $db->prepare('SELECT Password, MemberId FROM Member WHERE Email = ?');
+   //    $query1->execute([$username]);
+   //    $count = $query1->rowCount();
+   //    $row = $query1->fetch();
+   //    $password_h = $row['Password'];
 
-      // If result matched $myusername and $mypassword, table row must be 1 row
-      if($count == 1 && password_verify($password, $password_h)) {
-         $_SESSION['username'] = $username;
+   //    // If result matched $myusername and $mypassword, table row must be 1 row
+   //    if($count == 1 && password_verify($password, $password_h)) {
+   //       $_SESSION['username'] = $username;
 
-         header("Location: index.html");
-      }else {
-         $error = "Your Login Name or Password is invalid!";
-         echo $error;
-      }
-   }
+   //       header("Location: index.html");
+   //    }else {
+   //       $error = "Your Login Name or Password is invalid!";
+   //       echo $error;
+   //    }
+   // }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -43,7 +45,7 @@
   </head>
   <body>
     <h1>Sign In</h1>
-    <form action="<?PHP $_SERVER['PHP_SELF']; ?>" method="post">
+    <form action="../global/crud.php" method="post"> 
       <div>
         <span class="label">email: </span>
         <input type="text" name="email" value="">
@@ -53,8 +55,11 @@
         <input type="password" name="password" value="">
       </div>
       <div>
-        <input type="submit" name="submit" value="Submit">
+        <input type="submit" name="login" value="Submit">
       </div>
     </form>
   </body>
 </html>
+
+
+ <!--action="?PHP $_SERVER['PHP_SELF'] -->
